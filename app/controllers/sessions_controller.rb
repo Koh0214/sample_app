27be3c99ff30fcb9de667a098e1997da65d4ccab
Kohei0214 <kkishimotok@gmail.com>
2018-05-10 20:12:22 +0900

    add login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      flash[:danger] = "ユーザー情報間違っとるよ。"
+      render 'new'
+    end
+  end
 end
