31831b61092dc0705da0f784b1e0849e52dbbeed
Kohei0214 <kkishimotok@gmail.com>
2018-05-14 18:25:35 +0900

    update sessions_helper to add method to remember user cookie

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,13 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # ユーザーのセッションを永続的にする
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   def current_user
     #current＿userがいる場合はそのまま、いない場合はfind_byするというコード
     @current_user ||= User.find_by(id: session[:user_id])
