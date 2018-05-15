ef6813e1e6f380c9fd2c4eb7889e600a4edbb709
Kohei0214 <kkishimotok@gmail.com>
2018-05-16 00:07:21 +0900

    mail preview setting

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(version: 2018_05_15_142828) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.boolean "admin", default: false
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
